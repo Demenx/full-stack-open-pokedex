dc3ee5ffd75a9eddc0078eeff9cb73aa8011ef28
Memo <luis.machado.m@gmail.com>
2023-05-11 19:12:05 -0400

    ahora si que si

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,28 +2,35 @@ module.exports = {
   "env": {
     "browser": true,
     "es6": true,
+    "node": true, // Add this line if you're using Node.js
     "jest/globals": true
   },
-  'extends': 'eslint:recommended',
-    'parserOptions': {
-        'ecmaVersion': 'latest'
-    },
-    'rules': {
-        'indent': [
-            'error',
-            2
-        ],
-        'linebreak-style': [
-            'error',
-            'unix'
-        ],
-        'quotes': [
-            'error',
-            'single'
-        ],
-        'semi': [
-            'error',
-            'never'
-        ]
-    }
+  "plugins": [ // Add this section
+    "jest"
+  ],
+  "extends": [
+    'eslint:recommended',
+    'plugin:jest/recommended' // Add this line
+  ],
+  "parserOptions": {
+    "ecmaVersion": 'latest'
+  },
+  "rules": {
+    "indent": [
+      "error",
+      2
+    ],
+    "linebreak-style": [
+      "error",
+      "unix"
+    ],
+    "quotes": [
+      "error",
+      "single"
+    ],
+    "semi": [
+      "error",
+      "never"
+    ]
+  }
 }
